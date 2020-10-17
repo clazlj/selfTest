8bd0fa7087dcb6f03804a21a5604e8b8aa8341af
clazlj <584574197@qq.com>
2020-10-17 17:13:27 +0800

    守护线程

---
+++ src/main/java/demo/DaemonThreadDemo.java
@@ -0,0 +1,27 @@
+package demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 守护线程
+ * 最常见应用：垃圾回收器线程
+ */
+@Slf4j
+public class DaemonThreadDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                if (Thread.currentThread().isInterrupted()) {
+                    break;
+                }
+                log.debug("t1线程结束");
+            }
+        }, "t1");
+        //设置守护线程，进程结束时不管该线程是否结束都强制结束进程。
+        t1.setDaemon(true);
+        t1.start();
+
+        Thread.sleep(1_000);
+        log.debug("主线程结束");
+    }
+}
