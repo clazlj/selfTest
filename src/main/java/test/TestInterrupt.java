4186a4c19b8cf63f750ec5066e193b252d0d9865
clazlj <584574197@qq.com>
2020-10-17 14:47:54 +0800

    Priority

--- src/main/java/test/TestInterrupt.java
+++ src/main/java/test/TestInterrupt.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 
-@Slf4j
+@Slf4j(topic = "c.interrupt")
 public class TestInterrupt {
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread("t1") {
