3100a702b43c8a76277bbb86ed8eb8db8d33402b
clazlj <584574197@qq.com>
2020-10-17 14:16:29 +0800

    TestThreadSleep

---
+++ src/main/java/test/TestFrames.java
@@ -0,0 +1,29 @@
+package test;
+
+public class TestFrames {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> method1(20));
+        t1.setName("t1");
+        t1.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+            }
+        };
+
+        method1(10);
+    }
+
+    private static void method1(int x) {
+        int y = x + 1;
+        Object m = method2();
+        System.out.println(m);
+    }
+
+    private static Object method2() {
+        Object n = new Object();
+        return n;
+    }
+}
---
+++ src/main/java/test/TestThreadSleep.java
@@ -0,0 +1,25 @@
+package test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestThreadSleep {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(2_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "t1");
+
+        t1.start();
+        log.debug("t1 state:{}", t1.getState());
+
+        Thread.sleep(500);
+        //sleep改变线程的状态为TIMED_WAITING
+        log.debug("t1 state:{}", t1.getState());
+
+
+    }
+}
