1a21f977741f88486380260888046aee60271a89
clazlj <584574197@qq.com>
2020-10-17 16:31:29 +0800

    打断阻塞，打断正常

--- src/main/java/test/TestInterrupt.java
+++ src/main/java/test/TestInterrupt.java
@@ -7,6 +7,16 @@ import java.util.concurrent.TimeUnit;
 @Slf4j(topic = "c.interrupt")
 public class TestInterrupt {
     public static void main(String[] args) throws InterruptedException {
+//        interruptBlock();
+
+        interruptNormal();
+    }
+
+    /**
+     * 打断阻塞的线程，sleep,wait,join等
+     * @throws InterruptedException
+     */
+    private static void interruptBlock() throws InterruptedException {
         Thread t1 = new Thread("t1") {
             @Override
             public void run() {
@@ -28,6 +38,25 @@ public class TestInterrupt {
         TimeUnit.SECONDS.sleep(1);
 
         log.debug("interrupt...");
+        t1.interrupt();
+    }
+
+    private static void interruptNormal() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                boolean interrupted = Thread.currentThread().isInterrupted();
+                if (interrupted) {
+                    log.debug("被打断了，退出循环");
+                    break;
+                }
+            }
+        }, "t1");
+        t1.start();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        log.debug("interrupt");
+        //t1知道自己被打断了，但实际还在执行。可以自己获取打断标记
         t1.interrupt();
     }
 }
