8e1c997b497f1f7bbdeebc7dc6e28345da0b801e
caoliang <caoliang@jiangduoduo.com>
2020-11-20 15:11:28 +0800

    0

--- src/main/java/demo/VolatileAtomicThreadDemo.java
+++ src/main/java/demo/VolatileAtomicThreadDemo.java
@@ -1,10 +1,13 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class VolatileAtomicThreadDemo implements Runnable {
     // 定义一个int类型的遍历
-    private static volatile int count = 0;
+    public volatile int count = 0;
 
     private AtomicInteger atomicInteger = new AtomicInteger();
 
@@ -13,7 +16,7 @@ public class VolatileAtomicThreadDemo implements Runnable {
         // 对该变量进行++操作，100次
         for(int x = 0 ; x < 100 ; x++) {
             count++ ;
-            System.out.println("count =========>>>> " + count);
+//            System.out.println("count =========>>>> " + count);
         }
     }
 
@@ -33,9 +36,19 @@ public class VolatileAtomicThreadDemo implements Runnable {
         // 创建VolatileAtomicThread对象
         VolatileAtomicThreadDemo volatileAtomicThreadDemo = new VolatileAtomicThreadDemo();
         // 开启100个线程对count进行++操作
+        List<Thread> threadList = new ArrayList<>();
         for (int x = 0; x < 100; x++) {
-            new Thread(volatileAtomicThreadDemo).start();
+            threadList.add(new Thread(volatileAtomicThreadDemo));
         }
+        threadList.forEach(Thread::start);
+
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(volatileAtomicThreadDemo.count);
+
     }
 }
 
