cdb475cf14b8b9d56e5387464fdfc330296c5e6a
caoliang <caoliang@jiangduoduo.com>
2021-01-15 19:01:39 +0800

    \u0000 转换为int类型表示 0 。空格字符 转换为int类型表示 32 。

--- src/main/java/demo/CharDemo.java
+++ src/main/java/demo/CharDemo.java
@@ -2,6 +2,7 @@ package demo;
 
 /**
  * Java中 char 类型的系统默认值是空 ， \u0000 代表字符为空 转换为int类型表示 0 。
+ * 空格字符 转换为int类型表示 32 。
  */
 public class CharDemo {
     private static char myChar;
@@ -18,5 +19,14 @@ public class CharDemo {
 
         //没有空格，无法替换
         System.out.println(String.valueOf(myChar).replace(" ", ""));
+
+
+        //\u0000 转换为int类型表示 0 。
+        //空格字符 转换为int类型表示 32 。
+        char emptyChar = ' ';
+        System.out.println((int) emptyChar);
+
+        char nullChar = '\u0000';
+        System.out.println((int) nullChar);
     }
 }
