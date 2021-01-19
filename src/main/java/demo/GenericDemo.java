a3d5790ebddc9b29660d7993ca34204bffc02baf
caoliang <caoliang@jiangduoduo.com>
2021-01-19 17:49:33 +0800

    Java的泛型是伪泛型：新增验证方法

--- src/main/java/demo/GenericDemo.java
+++ src/main/java/demo/GenericDemo.java
@@ -9,9 +9,26 @@ import java.util.List;
  * Java的泛型是伪泛型。
  * Java在编译期间，所有的泛型信息都会被擦掉。
  * 这就是通常所说的泛型擦除。
+ * 可以理解为，Java集合中的泛型，是用于防止错误类型元素输入的。
  */
 public class GenericDemo {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        compareClass();
+
+        addElement();
+    }
+
+    private static void compareClass() {
+        List<Integer> intList = new ArrayList<Integer>();
+        List<String> strList = new ArrayList<String>();
+
+        Class<? extends List> intListClass = intList.getClass();
+        Class<? extends List> strListClass = strList.getClass();
+
+        System.out.println("intListClass与strListClass" + (intListClass == strListClass ? "相等" : "不相等"));
+    }
+
+    private static void addElement() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         List<Integer> list = new ArrayList<>();
         list.add(3);
 
