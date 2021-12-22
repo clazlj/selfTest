8060d3b53c9a801fc38e0e7abf706a6df3f40dd4
caoliang <584574197@qq.com>
2021-12-22 23:47:54 +0800

    StringUtils换个包

--- src/main/java/org/caoliang/Main.java
+++ src/main/java/org/caoliang/Main.java
@@ -1,6 +1,6 @@
 package org.caoliang;
 
-import com.sun.deploy.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import service.*;
 import service.impl.*;
 import util.RedPacketUtils;
