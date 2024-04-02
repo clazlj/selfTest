a83d4aa1cc4395743b98060dc3741573f2e79f3b
caoliang <caoliang@yiqicai.com>
2024-04-02 17:54:47 +0800

    extendAddDemo,superAddDemo

--- src/main/java/demo/PECSDemo.java
+++ src/main/java/demo/PECSDemo.java
@@ -8,12 +8,41 @@ import java.util.List;
  * PECS（Producer Extends Consumer Super）原则：
  * 频繁往外读取内容的，适合用上界Extends。
  * 经常往里插入的，适合用下界Super。
+ *
+ * 上界生产，下界消费。
  */
 public class PECSDemo {
     public static void main(String[] args) {
+//        无限通配符<?>
+//        上界通配符<? extends E>
+//        下界通配符<? super E>
 
         typicalDemoWithCopy();
 
+        extendAddDemo();
+        superAddDemo();
+    }
+
+    private static void extendAddDemo() {
+        // ?可能是Worker，也可能是Programmer
+        // demo：如果是Programmer，此时加Worker是不能转为Programmer的
+        List<? extends Worker> workerList = new ArrayList<>();
+        // 编译报错
+//        workerList.add(new Worker());
+//        workerList.add(new Programmer());
+
+        // 唯一的例外：null。null可以是任意类的实例。
+        workerList.add(null);
+    }
+
+    private static void superAddDemo() {
+        // ?可能是Worker，Job，Object等。所以加Worker及其子类都可以
+        List<? super Worker> workerList = new ArrayList<>();
+        workerList.add(new Worker());
+        workerList.add(new Programmer());
+        workerList.add(null);
+        // 不能加Job实例，因为如果是Worker，没法转成Worker
+//        workerList.add(new Job());
     }
 
     private static void typicalDemoWithCopy() {
